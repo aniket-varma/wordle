76872c43077a18ad5dd857c9d3a345cc24c8529b
Aniket Varma <incredibleaniket2002@gmail.com>
2022-12-29 00:13:56 +0530

    Handling API Request

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const PORT=8000
-const axios=require("axios").default
-const express=require("express")
-const cors=require("cors")
-const app=express()
+const PORT = 8000;
+const axios=require("axios").default;
+const express=require("express");
+const cors=require("cors");
+const app=express();
+require('dotenv').config()
 app.use(cors())
 app.get('/word',(req,res)=>{
     const axios = require("axios");
-    app.listen(PORT,()=>{
-        console.log('Server running on port '+PORT);
-    })
     const options = {
-        method: 'GET',
-        url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
-        params: {count: '5', wordLength: '5'},
-        headers: {
-            'X-RapidAPI-Host': 'random-words5.p.rapidapi.com',
-            'X-RapidAPI-Key': 'c4df6b35f8msh4539e6ec83dbc21p1f0b62jsnc2526e2e784e'
-        }
+    method: 'GET',
+    url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
+    params: {count: '5', wordLength: '5'},
+    headers: {
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+        'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+    }
     };
 
     axios.request(options).then(function (response) {
-        console.log(response.data);
+        res.json(response.data[0]);
     }).catch(function (error) {
         console.error(error);
     });
 })
+
+app.listen(PORT, () => console.log(`Server running on port = ` + PORT));
--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const mes=document.querySelector('.message-container');
 const keys=[
     'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','ENTER','Z','X','C','V','B','N','M','DEL',
 ]
-const word='SUPER';
+let word;
+
+const getWord = () => {
+    fetch('http://localhost:8000/word').then(response => {
+        return response.json();
+    }).then(json => {
+        word=json.toUpperCase();
+    }).catch(err=> console.log(err));
+}
+getWord();
 let isGameOver=false;
 const guessRows=[
     ['','','','',''],
@@ -29,7 +38,7 @@ guessRows.forEach((guessRow,guessRowIndex)=>{
 
 })
 const handleClick=(k)=>{
-    if(k=='ENTER'){
+    if(k==='ENTER'){
         checkWord();
     }
     else if(k==='DEL'){
@@ -43,6 +52,15 @@ const handleClick=(k)=>{
     }
     else addLetter(k);
 }
+const clearRow = () => {
+    for(let i=0;i<guessRows[currentRow].length-1;i++){
+        const curr=document.getElementById('guessRow-'+currentRow+'-tile-'+i);
+        curr.textContent='';
+        guessRows[currentRow][currentCol]='';
+        curr.setAttribute('data','');
+    }
+    currentCol=0;
+}
 const addLetter=(letter)=>{
     if(currentRow<6 && currentCol<5){
         const curr=document.getElementById('guessRow-'+currentRow+'-tile-'+currentCol);
@@ -54,26 +72,32 @@ const addLetter=(letter)=>{
 }
 const checkWord=()=>{
     if(currentCol==5){
-        let str=''
-        guessRows[currentRow].forEach(l=>{
-            str=str+l
-        })
-        addColor();
-        if(str===word){
-            showMessage('Magnificient!');
-            isGameOver=true;
-            return;
-        }
-        else{
-            if(currentRow>=5){
-                isGameOver=false;
-                showMessage('Game Over! Word is '+word);
+        const guess=guessRows[currentRow].join('');
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`).then(response => response.json() ).then(json =>{
+            if(json[0]!==undefined){
+                let str=guess;
+                addColor();
+                if(str===word){
+                    showMessage('Magnificient!');
+                    isGameOver=true;
+                    return;
+                }
+                else{
+                    if(currentRow>=5){
+                        isGameOver=false;
+                        showMessage('Game Over! Word is '+word);
+                    }
+                    else{
+                        currentRow++;
+                        currentCol=0;
+                    }
+                }
             }
             else{
-                currentRow++;
-                currentCol=0;
+                showMessage(`Oops! ${guess} is not a word.`)
+                clearRow();
             }
-        }
+        })
     }
 }
 const showMessage=(message)=>{
@@ -81,9 +105,8 @@ const showMessage=(message)=>{
     messageElement.textContent=message;
     mes.append(messageElement);
     setTimeout(()=>mes.removeChild(messageElement),2000);
-
-
 }
+
 keys.forEach(k=>{
     const buttonElement=document.createElement('button');
     buttonElement.textContent=k;
@@ -97,7 +120,6 @@ const addColor=()=>{
     let checkWord=word;
     ti.forEach((t,ind)=>{
         const letter=t.getAttribute('data');
-        
         const x=document.querySelector('#'+letter);
         if(letter==checkWord[ind]){
             t.classList.add('flip');
@@ -113,7 +135,6 @@ const addColor=()=>{
         const x=document.querySelector('#'+letter);
         t.classList.add('flip');
         x.classList.add('flip');
-        console.log(checkWord);
         if(checkWord.includes(letter)){
             t.classList.add('yellow-col');
             x.classList.add('yellow-col');
